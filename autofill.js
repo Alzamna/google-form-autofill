79921fe35f1c0c0e238ead63d7570c3b6bf77652
Alzamna <165247202+Alzamna@users.noreply.github.com>
2025-07-14 09:30:44 +0700

    Create autofill.js

---
+++ autofill.js
@@ -0,0 +1,16 @@
+const radios = document.querySelectorAll('div[role="radio"]');
+let selected = false;
+
+radios.forEach(e => {
+    const pos = parseInt(e.getAttribute('aria-posinset'));
+    const random = Math.random();
+
+    if ((pos === 3 && random < 0.7) || (pos === 4 && random < 0.7) || random < 0.3) {
+        e.click();
+        selected = true;
+    }
+});
+
+if (!selected && radios.length > 0) {
+    radios[Math.floor(Math.random() * radios.length)].click();
+}
