f5ba4ffc76ad10f868f34d4e88642ac1a252d5bd
Alzamna <165247202+Alzamna@users.noreply.github.com>
2025-07-14 10:14:59 +0700

    Update textarea.js

--- textarea.js
+++ textarea.js
@@ -1,4 +1,3 @@
-// SCRIPT: Isi semua textarea
 document.querySelectorAll('textarea').forEach((el, i) => {
     const text = `Jawaban panjang random ke-${i + 1}: Lorem ipsum dolor sit amet, consectetur adipiscing elit.`;
     el.value = text;
